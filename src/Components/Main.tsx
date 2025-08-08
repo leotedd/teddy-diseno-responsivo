c01e6f2d5a5d3b97e4f1f9ead8f7fa61a71f7b80
teddyhernandezp02@gmail.com <teddyhernandezp02@gmail.com>
2025-08-08 04:13:32 -0600

    ingrese modficaciones en el Main.tsx

---
+++ src/Components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main: React.FC = () => (
+  <main className="container text-center my-5">
+    <h1>Electricidad</h1>
+    <p className="lead">
+      Brindamos soluciones eléctricas.
+    </p>
+  </main>
+);
+
+export default Main;
